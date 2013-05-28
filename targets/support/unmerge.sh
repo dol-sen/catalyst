aed5c791869c1a62a42d9e1a72d339ddedbf501e
Brian Dolbec <dolsen@gentoo.org>
2013-05-27 17:18:43 -0700

    Make the use of preserved_libs optional by setting the options variable.
    Ported from commits:
     462348d88b3a5fa783f322c64131025581ecc220
     f6ad384914a00099eea2a0f66232728c479ec628
     in the master branch.
    AUTHOR: Jorge Manuel B. S. Vicetto (jmbsvicetto) <jmbsvicetto@gentoo.org> (Sun 14 Apr 2013 08:22:34 PM PDT)

--- targets/support/unmerge.sh
+++ targets/support/unmerge.sh
@@ -2,6 +2,8 @@
 
 source /tmp/chroot-functions.sh
 
+# If the user enabled PRESERVE_LIBS in options, tell portage to preserve them.
+[ -n "${clst_PRESERVE_LIBS}" ] && FEATURES="${clst_myfeatures} preserve-libs"
 run_merge -C ${clst_packages}
 
 exit 0
